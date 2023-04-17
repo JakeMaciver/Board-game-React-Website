8f12b27f519ca5cea68dcb36377e690247122326
JakeMaciver <Jacobmaciverwork@gmail.com>
2023-04-17 17:17:00 +0100

    add error handling and loading UX for api calls on all reviews

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,19 +5,31 @@ import { getReviews } from "./api";
 export const Content = () => {
 
   const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getReviews();
-      console.log(data);
-      setReviews(data);
-    }
-    fetchData();
-  }, [])
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const data = await getReviews();
+				setReviews(data);
+			} catch (error) {
+				setError('Error fetching data from api');
+			}
+			setIsLoading(false);
+		};
+		fetchData();
+	}, []);
 
-  return (<section className="main-content">
-    <ul className="cards-list">
-      {createReviewCards(reviews)}
-    </ul>
-  </section>)
+	return (
+		<section className='main-content'>
+			{isLoading ? (
+				<p className="loading-text">Loading content...</p>
+			) : error ? (
+				<p>{error}</p>
+			) : (
+				<ul className='cards-list'>{createReviewCards(reviews)}</ul>
+			)}
+		</section>
+	);
 }
