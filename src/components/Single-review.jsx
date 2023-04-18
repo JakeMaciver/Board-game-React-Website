b65e11de1f47683f7121ee71109a9482745510ee
JakeMaciver <Jacobmaciverwork@gmail.com>
2023-04-18 11:12:15 +0100

    format documents using prettier

--- src/components/Single-review.jsx
+++ src/components/Single-review.jsx
@@ -1,26 +1,25 @@
-import { useEffect, useState } from "react";
-import {useParams} from "react-router-dom";
-import { getSingleReview } from "./api";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getSingleReview } from './api';
 
 export const SingleReview = () => {
+	const [review, setReview] = useState([]);
+	let { review_id } = useParams();
+	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState(null);
 
-  const [review, setReview] = useState([]);
-  let {review_id} = useParams();
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-      const fetchData = async () => {
-				try {
-					const data = await getSingleReview(review_id);
-					setReview(data);
-				} catch (error) {
-					setError('Error fetching data from api');
-				}
-				setIsLoading(false);
-			};
-			fetchData();
-  }, [review_id])
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const data = await getSingleReview(review_id);
+				setReview(data);
+			} catch (error) {
+				setError('Error fetching data from api');
+			}
+			setIsLoading(false);
+		};
+		fetchData();
+	}, [review_id]);
 
 	return (
 		<ul className='single-review'>
