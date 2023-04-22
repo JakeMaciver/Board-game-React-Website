c8cb34b926bdbc100f4b699a8f9470247431c93c
JakeMaciver <Jacobmaciverwork@gmail.com>
2023-04-22 14:11:48 +0100

    change styling for menu when screen size is less than 1020px

--- src/components/Top-bar.jsx
+++ src/components/Top-bar.jsx
@@ -6,17 +6,22 @@ export const Topbar = ({setSidebarVisible, screenWidth}) => {
 
   return (
 		<section className='top-bar'>
-			<section className='menu-icon'>
-				<span className='material-symbols-outlined' onClick={handleMenuOnClick}>
-					menu
-				</span>
+			{screenWidth > 1020 ? null : (
+				<section className='menu-icon'>
+					<span
+						className='material-symbols-outlined'
+						onClick={handleMenuOnClick}
+					>
+						menu
+					</span>
+				</section>
+			)}
+			<section className='create-review-btn'>
+				<button className='create-review-btn'>Create A Review</button>
 			</section>
-      <section className="create-review-btn">
-			<button className='create-review-btn'>Create A Review</button>
-      </section>
-      <section className="account-icon">
-			<span className='material-symbols-outlined'>account_circle</span>
-      </section>
+			<section className='account-icon'>
+				<span className='material-symbols-outlined'>account_circle</span>
+			</section>
 		</section>
 	);
 }
